0e985e5d053d3fa258f6f4b0130573c8ee6ae686
FrozenInc <kristyan.lazarov@gmail.com>
2017-04-15 11:24:29 +0200

    made basic code

--- sketch_LazarusMail.ino
+++ sketch_LazarusMail.ino
@@ -1,9 +1,45 @@
+// This is the code for the Arduino controlling Lazarus mail
+// It has the following parts:
+// 3 digital sensors
+// 1 analog sensor
+// 1 stepper motor with coresponding controller accepting: direction, steps, sleep, reset
+
+
+// Setup is not needed as eveything is determinated in the loop at every run
 void setup() {
   // put your setup code here, to run once:
 
 }
 
+// Loop checks all the sensors and decides if the elevator should move and at what direction
 void loop() {
   // put your main code here, to run repeatedly:
 
 }
+
+
+int elevatorTopSensor()
+{
+  
+}
+
+int elevatorBottomSensor()
+{
+    
+}
+
+int openingSensor()
+{
+  
+}
+
+int boxSensor()
+{
+  
+}
+
+int stepperMotor(int dir, int steps)
+{
+  
+}
+
